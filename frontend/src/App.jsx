9aeb9c286821a7966a06a259f3d7cd29b1bd328c
Tyler Mestery <tyler@mestery.com>
2025-10-23 21:50:21 -0500

    Display home page UI.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import "./App.css"
-import MovieCard from "./components/MovieCard.jsx"
+import "./App.css";
+import MovieCard from "./components/MovieCard.jsx";
+import Home from  "./pages/Home.jsx";
 
 function App() {
   return (
-    <>
-      <MovieCard movie={{title: "Terrifier 2", release_date: "2024"}}/>
-      <MovieCard movie={{title: "Terrifier", release_date: "2013"}}/>
-    </>
-  )
+      <Home />
+  );
 }
 
 export default App
